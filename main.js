5d7371988f0b2ca7d926f35b4bdffa5635298a74
iamtanjil <tanjil4work@gmail.com>
2022-09-03 00:27:48 +0600

    category section complete

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('conneted');
+const getCategoryData = () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setCategoryData(data.data.news_category))
+};
+const setCategoryData = (datas) => {
+    datas.forEach(data => {
+        const categoryDiv = document.getElementById('category');
+        const div = document.createElement('h1');
+        div.innerHTML = `
+            <button class="hover:text-purple-700">${data.category_name}</button>
+        `;
+        categoryDiv.appendChild(div);
+    });
+};
+getCategoryData();
